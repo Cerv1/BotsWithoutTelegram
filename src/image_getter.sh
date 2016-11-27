af3e7e9a14ff1f5fa625502593accad2b74f673e
Cerv1 <seryi_111@hotmail.es>
2016-11-27 12:25:34 +0100

    First steps into LUA scripting.

--- src/image_getter.sh
+++ src/image_getter.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-echo "Iniciado script"
+# echo "Iniciado script"
+#
+# cd ~/tg/bin/
+#
+# (
+# echo "contact_search ImageBot";sleep 2;
+# echo "msg ImageBot /get $1"; sleep 4;
+# echo "load_file 2"; sleep 5;
+#
+# ) | ./telegram-cli -N
+#
+# eog -f ~/.telegram-cli/downloads/*
+#
+# rm -r ~/.telegram-cli/downloads
 
-cd ~/tg/bin/
+cd ~/tg/bin
 
-(
-echo "contact_search ImageBot";sleep 2;
-echo "msg ImageBot /get $1"; sleep 4;
-echo "load_file 2"; sleep 5;
-
-) | ./telegram-cli -N
-
-eog -f ~/.telegram-cli/downloads/*
-
-rm -r ~/.telegram-cli/downloads/*
+./telegram-cli -W
+--exec "msg Adri 1";
+--exec "msg Adri 2";
