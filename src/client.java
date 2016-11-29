f1e7490ce89fa46b884e630ba2921c1ec5a8a02b
Adrian Morente <adrian95morente@gmail.com>
2016-11-29 13:20:53 +0100

    añadiendo menú de opciones al cliente

--- src/client.java
+++ src/client.java
@@ -17,29 +17,62 @@ class Client{
   int port=4444;
 
   class IncomingClient{
+
+    public String chooseOption(){
+      String option = "0";
+      while(option!="1" || option!="2"){
+        System.out.println("\nWelcome! I'm a nice server, this is what I can do: \n");
+        System.out.println("\t\t[1]: Show an image.");
+        System.out.println("\t\t[2]: Show a GIF.");
+        // System.out.println("\t\t[3]: I'm just thinking...");
+        Console console = System.console();
+        option = console.readLine("\nWatchu wanna do? --> ");
+      }
+      return option;
+    }
+
+    public String chooseMessage(){
+      String option = "";
+      Console console = System.console();
+      option = console.readLine("\nFine! Now tell me what you want your image/GIF to be about: ");
+      return option;
+    }
+
     public void runListener(){
       try{
+        chooseOption();
         System.out.println("Connecting to server:"+serverName+" Port:"+port);
         client = new Socket(serverName,port);
         System.out.println("Connected to: "+client.getRemoteSocketAddress());
         while(true){
           DataOutputStream output = new DataOutputStream(client.getOutputStream());
-          Console console = System.console();
-          String target = console.readLine("You want an image about... ");
-          output.writeUTF(target);
-          output.flush();
-          DataInputStream input = new DataInputStream(client.getInputStream());
 
-          FileOutputStream image = new FileOutputStream("/home/adri/Descargas/Telegram-CLI/socket-image.jpg");
-          // FileOutputStream image = new FileOutputStream("/home/adri/Descargas/Telegram-CLI/socket-image.mp4");
-
-          int i;
-          while((i = input.read()) > -1){
-            image.write(i);
+          String option = chooseOption();
+          String message = chooseMessage();
+          if(option == "1"){
+            output.writeUTF(message);
+            output.flush();
+            DataInputStream input = new DataInputStream(client.getInputStream());
+            FileOutputStream image = new FileOutputStream("/home/adri/Descargas/Telegram-CLI/socket-image.jpg");
+            int i;
+            while((i = input.read()) > -1)
+              image.write(i);
+            image.flush();
+            image.close();
+            input.close();
           }
-          image.flush();
-          image.close();
-          input.close();
+          else if(option == "2"){
+            output.writeUTF(message);
+            output.flush();
+            DataInputStream input = new DataInputStream(client.getInputStream());
+            FileOutputStream file = new FileOutputStream("/home/adri/Descargas/Telegram-CLI/socket-image.mp4");
+            int i;
+            while((i = input.read()) > -1)
+              file.write(i);
+            file.flush();
+            file.close();
+            input.close();
+          }
           // client.close();
         }
       }catch(Exception e){
