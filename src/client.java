5b83b0311969096b3d8f7bc9f68b22050c9a3036
Adrian Morente <adrian95morente@gmail.com>
2016-11-30 11:32:02 +0100

    fixing client side

--- src/client.java
+++ src/client.java
@@ -5,79 +5,63 @@ import java.net.Socket;
 import javax.imageio.*;
 import javax.swing.*;
 
-class Client{
+class Client{    //client side. the one who asks for a pic and receives it
 
   public Client() throws IOException{
-    ic.runListener();
+    ic.runListener();   //start looking for a pic
   }
 
   private IncomingClient ic = new IncomingClient();
   Socket client;
-  String serverName = "192.168.1.107";
-  int port=4444;
+  String serverName = "25.22.48.56";    //server IP direction
+  int port = 4444;    //server authorized port
 
   class IncomingClient{
-
-    // public String chooseOption(){
-      // String option = "0";
-      // while(option!="1" || option!="2"){
-      //   System.out.println("\nWelcome! I'm a nice server, this is what I can do: \n");
-      //   System.out.println("\t\t[1]: Show an image.");
-      //   System.out.println("\t\t[2]: Show a GIF.");
-      //   // System.out.println("\t\t[3]: I'm just thinking...");
-      //   Console console = System.console();
-      //   option = console.readLine("\nWatchu wanna do? --> ");
-      // }
-      // return option;
-    // }
 
     public String chooseMessage(){
       String option = "";
       Console console = System.console();
-      option = console.readLine("\nFine! Now tell me what you want your image/GIF to be about: ");
+      option = console.readLine("\nHi! Tell me what you want your image to be about: ");
       return option;
+    }
+
+    public void cleanAll(){
+      String dirPath = "/home/adri/Descargas/Telegram-CLI/";
+      File dir = new File(dirPath);
+      File[] files = dir.listFiles();
+      for(File aFile : files)
+        aFile.delete();
+
+      boolean deleted = dir.delete();
+      if(deleted)
+        System.out.println("Images removed...");
+      else
+        System.out.println("Couldn't clean...");
     }
 
     public void runListener(){
       try{
-        // chooseOption();
-        System.out.println("Connecting to server:"+serverName+" Port:"+port);
-        client = new Socket(serverName,port);
-        System.out.println("Connected to: "+client.getRemoteSocketAddress());
-        // while(true){
-          DataOutputStream output = new DataOutputStream(client.getOutputStream());
+        System.out.println("Connecting to server: "+serverName+"; Port: "+port);    //
+        client = new Socket(serverName,port);                                       //  connection with server
+        System.out.println("Connected to: "+client.getRemoteSocketAddress());       //
 
-          // String option = chooseOption();
-          String message = chooseMessage();
-          // if(option == "1"){
-          //   output.writeUTF(message);
-          //   output.flush();
-          //   DataInputStream input = new DataInputStream(client.getInputStream());
-          //   FileOutputStream image = new FileOutputStream("/home/cervi/Descargas/Telegram-CLI/socket-image.jpg");
-          //   int i;
-          //   while((i = input.read()) > -1)
-          //     image.write(i);
-          //   image.flush();
-          //   image.close();
-          //   input.close();
-          // }
-          // else if(option == "2"){
-            output.writeUTF(message);
-            output.flush();
-            DataInputStream input = new DataInputStream(client.getInputStream());
-            FileOutputStream file = new FileOutputStream("/home/cervi/Descargas/Telegram-CLI/socket-image.jpg");
-            int i;
-            while((i = input.read()) > -1)
-              file.write(i);
-            file.flush();
-            file.close();
-            input.close();
+        //type of object to contain the received image
+        DataOutputStream output = new DataOutputStream(client.getOutputStream());
+        String message = chooseMessage();
+        output.writeUTF(message);   //choose theme related image
+        output.flush();             //
+        DataInputStream input = new DataInputStream(client.getInputStream());
+        FileOutputStream file = new FileOutputStream("/home/adri/Descargas/Telegram-CLI/socket-image.jpg");
 
-            System.out.println("Image received.");
+        int i;
+        while((i = input.read()) > -1)
+          file.write(i);
 
-          // }
-          client.close();
-        // }
+        file.flush();
+        file.close();
+        input.close();
+        client.close();
+
       }catch(Exception e){
         e.printStackTrace();
       }
